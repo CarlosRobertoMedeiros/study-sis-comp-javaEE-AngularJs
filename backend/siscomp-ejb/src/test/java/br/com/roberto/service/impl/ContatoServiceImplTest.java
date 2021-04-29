ce3fa9b98b0d77cabae23a37ce13b7c9852eaf08
Carlos Roberto <carlosmedeiroslima@gmail.com>
2021-04-29 00:09:17 -0300

    Correção do pom para implementar o sure-fire

--- backend/siscomp-ejb/src/test/java/br/com/roberto/service/impl/ContatoServiceImplTest.java
+++ backend/siscomp-ejb/src/test/java/br/com/roberto/service/impl/ContatoServiceImplTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -26,14 +27,16 @@ public class ContatoServiceImplTest{
     private ContatoRepository contatoRepository = null;
 
     private void carregaContatosMocks(){
-        contatosMocks.add(new Contato(Long.valueOf(1),"06689401073","Antonio Nunes","2198745898"));
-        contatosMocks.add(new Contato(Long.valueOf(2),"49900370066","Lucia Da Silva","6158741236"));
-        contatosMocks.add(new Contato(Long.valueOf(3),"31619396033","Thiago Pereira","3194753678"));
+        this.contatosMocks = new ArrayList<>();
+        this.contatosMocks.add(new Contato(Long.valueOf(1),"06689401073","Antonio Nunes","2198745898"));
+        this.contatosMocks.add(new Contato(Long.valueOf(2),"49900370066","Lucia Da Silva","6158741236"));
+        this.contatosMocks.add(new Contato(Long.valueOf(3),"31619396033","Thiago Pereira","3194753678"));
     }
 
     @Before
     public void setUp(){
         MockitoAnnotations.initMocks(this);
+        this.carregaContatosMocks();
         Mockito.when(contatoRepository.findAll()).thenReturn(contatosMocks);
     }
 
@@ -46,7 +49,7 @@ public class ContatoServiceImplTest{
             contatosDtoMocks.add(new ContatoDto(contato.getId(),contato.getCpf(),contato.getNome(),contato.getTelefone()));
         });
 
-        //assertEquals(contatoInterno.size(),3);
+        assertEquals(contatoInterno.size(),3);
     }
 
 //    public ContatosPaginadosDto getContatosPaginados(int totalRegistrosPorPagina, int paginaAtual) {
